a6c97420237a420eac317cc48833afd7c955ef4d
aleenaahmad1 <aleenat31@gmail.com>
2023-05-02 21:31:00 +0500

    Card Layout

--- donation-platform/src/Homepage.jsx
+++ donation-platform/src/Homepage.jsx
@@ -1,3 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
 export default function Homepage(){
     //database se aaega
     const org_array = [
@@ -5,18 +12,82 @@ export default function Homepage(){
     description: "slay"},
     {name: "Aleena FOundation",
     description: "yummy"
+    },{name: "Edhi Foundation",
+    description: "slay"},
+    {name: "Aleena FOundation",
+    description: "yummy"
+    },{name: "Edhi Foundation",
+    description: "slay"},
+    {name: "Aleena FOundation",
+    description: "yummy"
     }
     ]
 
-    const orgdata = org_array.map(org => (
-        <>
-        <h1>{org.name}</h1>
-        <p>{org.description}</p>
-        </>
-    ))
+    const orgData=[]
+    const totalrows = org_array.length/3
+
+
+    for (let i=0; i<totalrows; i++){
+        orgData.push(
+        <Row>
+        <Col>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="Edhi-foundation.png" />
+        <Card.Body>
+            <Card.Title>{org_array[((i+1)*3)-3].name}</Card.Title>
+            <Card.Text>
+            {org_array[((i+1)*3)-3].description}
+            </Card.Text>
+            <Button variant="primary">Read Mores</Button>
+        </Card.Body>
+        </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="Edhi-foundation.png" />
+        <Card.Body>
+            <Card.Title>{org_array[((i+1)*3)-2].name}</Card.Title>
+            <Card.Text>
+            {org_array[((i+1)*3)-2].description}
+            </Card.Text>
+            <Button variant="primary">Read Mores</Button>
+        </Card.Body>
+        </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="Edhi-foundation.png" />
+        <Card.Body>
+            <Card.Title>{org_array[((i+1)*3)-1].name}</Card.Title>
+            <Card.Text>
+            {org_array[((i+1)*3)-1].description}
+            </Card.Text>
+            <Button variant="primary">Read Mores</Button>
+        </Card.Body>
+        </Card>
+        </Col>
+        </Row>
+        )
+    }
+
+
+    // const orgdata = org_array.map(org => (
+    //     <>
+    //     <Card style={{ width: '18rem' }}>
+    //     <Card.Img variant="top" src="Edhi-foundation.png" />
+    //     <Card.Body>
+    //         <Card.Title>{org.name}</Card.Title>
+    //         <Card.Text>
+    //         {org.description}
+    //         </Card.Text>
+    //         <Button variant="primary">Read Mores</Button>
+    //     </Card.Body>
+    //     </Card>
+    //     </>
+    // ))
     return(
     <>
-        {orgdata}
+        {orgData}
     </>
     )
 }
