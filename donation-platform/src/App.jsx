05476eeb0f17a7dd8ad5f2ffa5a15926f33bac03
Qudsia tariw <tariq.qudsia18@gmail.com>
2023-05-08 03:20:24 +0500

    homepage icons

--- donation-platform/src/App.jsx
+++ donation-platform/src/App.jsx
@@ -8,6 +8,7 @@ import Col from 'react-bootstrap/Col';
 import Login from './Login';
 import Aboutus from './aboutus';
 import Donationinfo from './Donationinfo';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,7 +27,11 @@ function App() {
       <br></br><br></br>
       <Aboutus></Aboutus>
       <br></br>
-      <button type="button" className="btn btn-info" onClick={()=>{handlecards()}}>See Donation Information</button>
+      <Button type="button" size="lg" className="btn btn-info" onClick={()=>{handlecards()}}>Donation Data
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" style={{marginLeft: '5px'}} fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
+        <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z"/>
+        </svg>
+      </Button>
       {cards&&(<DonationCards/>)}
     </>
     
